f7bc631ea1f708376df99778426d02959bab2e92
yaswanthgollu <yaswanthgollu@gmail.com>
2025-07-15 12:59:26 +0530

    port number updated to deploy on render.

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def recommend(movie):
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(debug=False, host='0.0.0.0', port=port)
 
 
